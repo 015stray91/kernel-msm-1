MotorolaMobilityLLC/kernel-msm

suspend: Print subsystem stats in kernel log
Instead of showing subsystem's sleep time by reading system file
/sys/kernel/debug/qcom_sleep_stats, show them in kernel log.
Mot-CRs-fixed: (CR)

Change-Id: Iaa17b519aa6194437e52cb9a258ca6c4775686ef
Signed-off-by: xuxj <xuxj12@motorola.com>
Reviewed-on: https://gerrit.mot.com/2781370
SME-Granted: SME Approvals Granted
SLTApproved: Slta Waiver
Tested-by: Jira Key
Reviewed-by: Yonghua Yan <yanyh2@motorola.com>
Reviewed-by: Zhenxin Xi <xizx@motorola.com>
Submit-Approved: Jira Key

drivers/soc/qcom/qcom_stats.c
@@ -98,6 +98,9 @@ struct subsystem_data {
 	u32 smem_item;
 	u32 pid;
 	bool not_present;
+#ifdef CONFIG_SUSPEND_DEBUG_MODULE
+	bool debugfs;
+#endif
 };
 
 static struct subsystem_data subsystems[] = {
@@ -700,6 +703,55 @@ int cx_stats_get_ss_vote_info(int ss_count,
 }
 EXPORT_SYMBOL(cx_stats_get_ss_vote_info);
 
+#ifdef CONFIG_SUSPEND_DEBUG_MODULE
+static ssize_t print_supsystem_sleep_stats(int i, struct sleep_stats *stat,
+				const char *name)
+{
+	static uint64_t prev_duration[ARRAY_SIZE(subsystems)];
+	u64 accumulated = stat->accumulated;
+	uint64_t delta_duration;
+	/*
+	 * If a subsystem is in sleep when reading the sleep stats adjust
+	 * the accumulated sleep duration to show actual sleep time.
+	 */
+	if (stat->last_entered_at > stat->last_exited_at)
+		accumulated += arch_timer_read_counter()
+			       - stat->last_entered_at;
+
+	delta_duration = accumulated - prev_duration[i];
+	prev_duration[i] = accumulated;
+	return printk("%s:\tAccumulated Duration:0x%llx, %lu.%03lu\n\n",
+			name, accumulated, delta_duration / 19200000L, delta_duration % 19200000L * 1000 / 19200000L);
+}
+
+ssize_t show_subsystem_sleep_stats(void)
+{
+	ssize_t length = 0;
+	int i = 0;
+	struct subsystem_data *subsystem;
+	struct sleep_stats *stat;
+
+	for (i = 0; i < ARRAY_SIZE(subsystems); i++) {
+		subsystem = &subsystems[i];
+		if (!subsystem->debugfs)
+			continue;
+
+		stat = qcom_smem_get(subsystem->pid, subsystem->smem_item, NULL);
+		if (IS_ERR(stat))
+			continue;
+
+		length += print_supsystem_sleep_stats(i, stat,
+				subsystem->name);
+	}
+
+	return length;
+}
+
+EXPORT_SYMBOL_GPL(show_subsystem_sleep_stats);
+#endif
+
+
+
 static void qcom_print_stats(struct seq_file *s, const struct sleep_stats *stat)
 {
 	u64 accumulated = stat->accumulated;
@@ -961,6 +1013,9 @@ static void qcom_create_subsystem_stat_files(struct dentry *root,
 
 		for (j = 0; j < ARRAY_SIZE(subsystems); j++) {
 			if (!strcmp(subsystems[j].name, name)) {
+#ifdef CONFIG_SUSPEND_DEBUG_MODULE
+				subsystems[j].debugfs = true;
+#endif
 				debugfs_create_file(subsystems[j].name, 0400, root,
 						    (void *)&subsystems[j],
 						    &qcom_subsystem_sleep_stats_fops);

015stray91/kernel-msm-1

Change-Id: I3814743942e11dc0f4780b11e89c025cc0cac2d4
Signed-off-by: Xunjie Xu <xuxj12@motorola.com>
Reviewed-on: https://gerrit.mot.com/3036205
Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
(cherry picked from commit 5e8e24d4068f7206801b3c3ca787c98caf0dd8db)
Reviewed-on: https://gerrit.mot.com/3047016
Reviewed-by: Yiqiang Feng <fengyq4@motorola.com>
Reviewed-by: Huosheng Liao <liaohs@motorola.com>

drivers/soc/qcom/qcom_stats.c
@@ -99,6 +99,9 @@ struct subsystem_data {
 	u32 smem_item;
 	u32 pid;
 	bool not_present;
+#ifdef CONFIG_SUSPEND_DEBUG_MODULE
+	bool debugfs;
+#endif
 };
 
 static struct subsystem_data subsystems[] = {
@@ -724,6 +727,53 @@ int cx_stats_get_ss_vote_info(int ss_count,
 }
 EXPORT_SYMBOL_GPL(cx_stats_get_ss_vote_info);
 
+#ifdef CONFIG_SUSPEND_DEBUG_MODULE
+static ssize_t print_supsystem_sleep_stats(int i, struct sleep_stats *stat,
+				const char *name)
+{
+	static uint64_t prev_duration[ARRAY_SIZE(subsystems)];
+	u64 accumulated = stat->accumulated;
+	uint64_t delta_duration;
+	/*
+	 * If a subsystem is in sleep when reading the sleep stats adjust
+	 * the accumulated sleep duration to show actual sleep time.
+	 */
+	if (stat->last_entered_at > stat->last_exited_at)
+		accumulated += arch_timer_read_counter()
+			       - stat->last_entered_at;
+
+	delta_duration = accumulated - prev_duration[i];
+	prev_duration[i] = accumulated;
+	return printk("%s:\tAccumulated Duration:0x%llx, %llu.%03llu\n\n",
+			name, accumulated, delta_duration / 19200000L, delta_duration % 19200000L * 1000 / 19200000L);
+}
+
+ssize_t show_subsystem_sleep_stats(void)
+{
+	ssize_t length = 0;
+	int i = 0;
+	struct subsystem_data *subsystem;
+	struct sleep_stats *stat;
+
+	for (i = 0; i < ARRAY_SIZE(subsystems); i++) {
+		subsystem = &subsystems[i];
+		if (!subsystem->debugfs)
+			continue;
+
+		stat = qcom_smem_get(subsystem->pid, subsystem->smem_item, NULL);
+		if (IS_ERR(stat))
+			continue;
+
+		length += print_supsystem_sleep_stats(i, stat,
+				subsystem->name);
+	}
+
+	return length;
+}
+
+EXPORT_SYMBOL_GPL(show_subsystem_sleep_stats);
+#endif
+
 int ddr_stats_get_change_his(struct ddr_stats_change_his_info *ddr_his_info)
 {
 	static const char buf[MAX_MSG_LEN] = "{class: misc_debug, res: ddr_his}";
@@ -1042,6 +1092,9 @@ static void qcom_create_subsystem_stat_files(struct dentry *root,
 
 		for (j = 0; j < ARRAY_SIZE(subsystems); j++) {
 			if (!strcmp(subsystems[j].name, name)) {
+#ifdef CONFIG_SUSPEND_DEBUG_MODULE
+				subsystems[j].debugfs = true;
+#endif
 				debugfs_create_file(subsystems[j].name, 0400, root,
 						    (void *)&subsystems[j],
 						    &qcom_subsystem_sleep_stats_fops);